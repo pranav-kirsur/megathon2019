788ec14bfabe40471b5d48aa9a4d510cd94d950a
bhavyajeet <bhavyajeet29@gmail.com>
2019-09-29 03:44:35 +0530

    :

--- SimToTitle.py
+++ SimToTitle.py
@@ -1,10 +1,23 @@
+from nltk.stem import PorterStemmer 
+from nltk.tokenize import word_tokenize 
+
+
 def SimToTitle(title,sentences):
+    ps=PorterStemmer()
     titleArr = title.split()
+    ansArr=[]
+    for i in range(0,len(titleArr)):
+        titleArr[i] = ps.stem(titleArr[i])
+#    print(titleArr)
     for sentence in sentences :
         sentenceArr=sentence.split()
+        for i in range (0,len(sentenceArr)) :
+            sentenceArr[i] = ps.stem(sentenceArr[i])
         common = set(titleArr).intersection(set(sentenceArr))
-#        print(common)
-        return (len(common)/len)
+        ansArr.append(len(common)/len(sentenceArr))
+    return (ansArr)
 
-
-SimToTitle("Jai Murdered Ahish",["Jai was busy sucking dicks","Ahish started making jokes","Pranav laughed at the Murdered Ahish"])
+"""
+k=SimToTitle("Jai murdered Ahish",["Jai was busy sucking dicks","Ahish started making jokes","Pranav laughed at the murdered Ahish"])
+print(k)
+"""
