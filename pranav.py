0519f2edb7ac52e17a8e2cf6b7bff56ef194a2c8
pranav-kirsur <pranav.kirsur@gmail.com>
2019-09-29 03:12:30 +0530

    added superlative

---
+++ pranav.py
@@ -0,0 +1,17 @@
+import nltk
+#nltk.download('punkt')
+#nltk.download('averaged_perceptron_tagger')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+
+# TODO error handling for 0 sentence length
+def number_of_superlatives(sentence):
+    tokenized = word_tokenize(sentence)
+    pos_tagged = nltk.pos_tag(tokenized)
+    num = 0
+    for i in pos_tagged:
+        if i[1] == 'JJS' or i[1] == 'RBS':
+            num+=1
+    return num / len(sentence)
+
