8280de6fdf2ac3253e75acec876d83e15ecfcbe7
bhavyajeet <bhavyajeet29@gmail.com>
2019-09-29 10:11:50 +0530

    added keygen

--- keygen.py
+++ keygen.py
@@ -36,3 +36,28 @@ def generate_candidate_keys(noun_phrases):
     return possible_keys
 
         
+
+def get_best_key(keys,sentence):
+    tokenized = word_tokenize(sentence)
+    pos_tagged = nltk.pos_tag(tokenized)
+    grammar = "NP: {<DT>?<JJ>*<NN>}"
+    cp = nltk.RegexpParser(grammar)    
+    t=cp.parse(pos_tagged)
+    n_leaves = len(t.leaves())
+    leavepos = set(t.leaf_treeposition(n) for n in range(n_leaves))
+    mapping = {}
+    for pos in t.treepositions():
+        if pos not in leavepos:
+            mapping[t[pos].label()] = len(pos)
+    pairslist = []
+    for i in keys:
+        pairslist.append((mapping.get(i,10000),i))
+      #  print(pairslist)
+    pairslist.sort()
+    return pairslist[0][1]
+
+
+
+def generate_key_from_sentence(sentence):
+    return get_best_key(generate_candidate_keys(generate_key_list(sentence)),sentence)
+
